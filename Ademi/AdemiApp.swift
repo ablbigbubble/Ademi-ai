ba3ad0736c0531e2fcf4bed363c890f9fdc071fd
Abylaikhan <aboranzhan@icloud.com>
2026-03-19 23:43:50 +0500

    Initial Commit

---
+++ Ademi/AdemiApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdemiApp.swift
+//  Ademi
+//
+//  Created by Abylaikhan Boranzhan on 19.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct AdemiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
